665d7daefad08b211d1c69016d969bece47524e0
agascong <agascong@gmail.com>
2020-04-11 02:58:05 +0200

    added files

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-import pypokerengine
+from pypokerengine.api.game import setup_config, start_poker
 from fishplayer import FishPlayer
 from consoleplayer import ConsolePlayer
 from randomplayer import RandomPlayer
 
-print("hello world")
+config = setup_config(max_round=1, initial_stack=1000, small_blind_amount=20)
+config.register_player(name="f1", algorithm=FishPlayer())
+config.register_player(name="f2", algorithm=FishPlayer())
+config.register_player(name="f3", algorithm=FishPlayer())
+config.register_player(name="f4", algorithm=FishPlayer())
+config.register_player(name="f5", algorithm=RandomPlayer())
+config.register_player(name="r1", algorithm=RandomPlayer())
+config.register_player(name="r2", algorithm=RandomPlayer())
+config.register_player(name="r3", algorithm=RandomPlayer())
+config.register_player(name="c1", algorithm=ConsolePlayer())
+game_result = start_poker(config, verbose=1)
